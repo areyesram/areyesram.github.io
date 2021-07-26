0656e23923caf5164dbab9edaa2a63ca28579cca
areyesram <areyesram@hotmail.com>
2021-07-25 23:12:00 -0500

    vid

--- js/app.js
+++ js/app.js
@@ -9,12 +9,21 @@ function fillCourses() {
                 var title = $("<td>").append(
                     $("<a>")
                         .attr("href", "course.html#" + o.Code)
+                        .attr("target", "_blank")
                         .text(o.Title)
                 );
+                var topic = getTopicCode(o.Code);
                 b.append(
                     $("<tr>")
                         .append($("<td>").text(o.Code))
-                        .append($("<td>").text(getTopic(o.Code)))
+                        .append(
+                            $("<td>").append(
+                                $("<a>")
+                                    .attr("href", "courses-" + topic + ".html")
+                                    .attr("target", "_blank")
+                                    .text(getTopic(o.Code))
+                            )
+                        )
                         .append(title)
                         .append($("<td>").text(o.PreReq))
                 );
@@ -45,6 +54,7 @@ function fillCalendar() {
                         var title = $("<td>").append(
                             $("<a>")
                                 .attr("href", "course.html#" + o.Code)
+                                .attr("target", "_blank")
                                 .text(p.Title)
                         );
                         b.append(
@@ -67,6 +77,68 @@ function fillCalendar() {
     });
 }
 
+function loadCurso() {
+    var code = window.location.hash;
+    if (code.startsWith("#")) code = code.replace("#", "");
+    else window.location.replace("404.html");
+    $.ajax({
+        url: "api/courses.json",
+        success: function (data) {
+            var c = data.find(function (o) {
+                return o.Code == code;
+            });
+            $("#title").text(c.Title);
+            $("#code").text("[" + c.Code + "]");
+            var ul = $("#topics").find("ul");
+            if (c.Topics && c.Topics.length) {
+                $.each(c.Topics, function () {
+                    var topic = this;
+                    ul.append($("<li>").text(topic));
+                });
+            } else {
+                ul.append($("<li>").text("T.B.D."));
+            }
+            ul = $("#prereq").find("ul");
+            if (c.PreReq && c.PreReq.length) {
+                $.each(c.PreReq, function () {
+                    var prereq = this;
+                    var link = $("<a>")
+                        .attr("href", "#" + prereq)
+                        .text(prereq);
+                    link.on("click", function () {
+                        window.location.replace("#" + prereq);
+                        window.location.reload();
+                    });
+                    ul.append($("<li>").append(link));
+                });
+            } else {
+                ul.append($("<li>").text("Ninguno."));
+            }
+            $.ajax({
+                url: "api/calendar.json",
+                success: function (calendar) {
+                    var sessions = calendar.filter(function (o) {
+                        return o.Code == code;
+                    });
+                    ul = $("#sessions").find("ul");
+                    if (sessions && sessions.length) {
+                        $.each(sessions, function () {
+                            var course = this;
+                            var link = $("<a>")
+                                .attr("href", course.Session)
+                                .attr("target", "_blank")
+                                .text(moment(course.Date * 1000).format("DD [de] MMMM [de] YYYY"));
+                            ul.append($("<li>").append(link));
+                        });
+                    } else {
+                        ul.append($("<li>").text("Ninguno."));
+                    }
+                }
+            });
+        }
+    });
+}
+
 function getTopic(code) {
     return {
         CB: "Computación básica",
@@ -78,5 +150,9 @@ function getTopic(code) {
         Py: "Python",
         SQL: "SQL Server",
         Web: "Web Dev"
-    }[code.split("-")[0]];
+    }[getTopicCode(code)];
+}
+
+function getTopicCode(code) {
+    return code.split("-")[0];
 }
