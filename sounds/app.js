43bb6adb570ba3323849df68014fd1dd3b82626c
Alejandro Reyes <alejandro.reyes@abaton.com.mx>
2023-07-06 23:25:59 -0600

    sound

---
+++ sounds/app.js
@@ -0,0 +1,36 @@
+function loadSounds() {
+    for (let o of _snd) {
+        const audio = $("<audio>").append(
+            $("<source>").attr("src", `file/${o}.m4a`)
+        );
+        const button = $("<button>")
+            .append(audio)
+            .append($("<img>").attr("src", `file/${o}.jpg`));
+        button.on("click", () => audio[0].play());
+        $("#container").append(button);
+    }
+}
+
+const _snd = [
+    "rewind",
+    "wow",
+    "omg",
+    "cutg",
+    "hehe",
+    "quack",
+    "nope",
+    "buzzer",
+    "mariojump",
+    "robloxoof",
+    "huh",
+    "bruh",
+    "ausemer",
+    "ausvote",
+    "taco",
+    "laword",
+    "dun",
+    "batman",
+    "scream",
+    "wrong",
+    "curb"
+];
