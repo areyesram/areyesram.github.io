8d104dbdb0877883c9f25285b762a3e9bd5bf527
Alejandro Reyes <alejandro.reyes@abaton.com.mx>
2023-07-08 11:25:35 -0600

    sound

--- sounds/app.js
+++ sounds/app.js
@@ -3,20 +3,47 @@ function loadSounds() {
         const audio = $("<audio>").append(
             $("<source>").attr("src", `file/${o}.m4a`)
         );
+        audio.on("play", evt => {});
+        audio.on("ended", evt => {
+            const o = evt.currentTarget;
+            $(o).parent().find(".progress").css("width", "0");
+        });
+        audio.on("timeupdate", evt => {
+            const o = evt.currentTarget;
+            $(o)
+                .parent()
+                .find(".progress")
+                .css("width", (o.currentTime / o.duration) * 100 + "%");
+        });
         const button = $("<button>")
             .append(audio)
-            .append($("<img>").attr("src", `file/${o}.jpg`));
-        button.on("click", () => audio[0].play());
+            .append($("<img>").attr("src", `file/${o}.jpg`))
+            .append($("<div>").addClass("progress"));
+        button.on("click", () => {
+            const o = audio[0];
+            if (o.paused) {
+                o.play();
+            } else {
+                o.pause();
+                o.currentTime = 0;
+            }
+        });
         $("#container").append(button);
     }
 }
 
 const _snd = [
     "rewind",
+    "bob",
+    "tss",
     "wow",
+    "wassup",
     "omg",
     "cutg",
+    "letme",
     "hehe",
+    "dialup",
+    "winxperr",
     "quack",
     "nope",
     "buzzer",
@@ -34,6 +61,7 @@ const _snd = [
     "dun",
     "batman",
     "scream",
+    "gta",
     "fckd",
     "nani",
     "wrong",
